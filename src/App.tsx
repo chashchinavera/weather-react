ab6a70b54d721ef9a464e2a65d8fecd313a8a3ab
Vera Chashchina <chaschinavera@gmail.com>
2025-03-29 07:02:50 +0300

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { MonthStatistics } from "./pages/MonthStatistics/MonthStatistics";
+import { Header } from "./shared/Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Header />
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/month-statistics" Component={MonthStatistics} />
---
+++ src/shared/Header/Header.tsx
@@ -0,0 +1,51 @@
+import Select from "react-select";
+import s from "./Header.module.scss";
+import { HeaderLogoSvg } from "../../assets/icons/svg/HeaderLogoSvg";
+import { HeaderThemeChangerSvg } from "../../assets/icons/svg/HeaderThemeChangerSvg";
+
+export const Header = () => {
+  const options = [
+    { value: "Санкт-Петербург", label: "Санкт-Петербург" },
+    { value: "Москва", label: "Москва" },
+    { value: "Екатеринбург", label: "Екатеринбург" },
+    { value: "Новосибирск", label: "Новосибирск" },
+    { value: "Казань", label: "Казань" },
+    { value: "Челябинск", label: "Челябинск" },
+    { value: "Омск", label: "Омск" },
+    { value: "Самара", label: "Самара" },
+  ];
+
+  const colourStyles = {
+    control: (styles: any) => ({
+      ...styles,
+      backgroundColor: "rgba(71, 147, 255, 0.2)",
+      width: "194px",
+      height: "37px",
+      border: "none",
+      borderRadius: "10px",
+      zIndex: "100",
+      fontSize: "14px",
+    }),
+  };
+
+  return (
+    <header className={s.header}>
+      <div className={s.header__wrapper}>
+        <div className={s.header__logo}>
+          <HeaderLogoSvg />
+        </div>
+        <div className={s.header__title}>React weather</div>
+      </div>
+      <div className={s.header__wrapper}>
+        <div className={s.header__themeChanger}>
+          <HeaderThemeChangerSvg />
+        </div>
+        <Select
+          options={options}
+          styles={colourStyles}
+          defaultValue={options[0]}
+        />
+      </div>
+    </header>
+  );
+};
