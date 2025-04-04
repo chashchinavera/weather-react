7d2e3e7f7a150bd3d5f9243b456edef1bd9f80d2
Vera Chashchina <chaschinavera@gmail.com>
2025-04-04 19:21:17 +0300

    add saving theme in localStorage

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,15 +1,21 @@
 import { ReactNode, useState } from "react";
 import { Theme, ThemeContext } from "../context/ThemeContext";
 import { changeCssRootVariables } from "../model/ChangeCssRootVariables";
+import { storage } from "../model/Storage";
 
 interface Props {
   children: ReactNode;
 }
 
 export const ThemeProvider = ({ children, ...props }: Props) => {
-  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+  const [theme, setTheme] = useState<Theme>(
+    storage.getItem("theme") || Theme.LIGHT
+  );
+
+  changeCssRootVariables(theme);
 
   function toggleTheme(theme: Theme) {
+    storage.setItem("theme", theme);
     setTheme(theme);
     changeCssRootVariables(theme);
   }
