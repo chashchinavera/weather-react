114ee85caac4f50f097025276cc6c13e9c80f69b
Vera Chashchina <chaschinavera@gmail.com>
2025-09-28 19:14:57 +0300

    Reduce MonthStatistics path

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
-import { MonthStatistics } from "./pages/MonthStatistics/MonthStatistics";
 import { Header } from "./shared/Header/Header";
 import { Popup } from "./shared/Popup/Popup";
 
 function App() {
   return (
     <>
-      <Popup />
+      {/* <Popup /> */}
       <div className="app">
         <Header />
         <Routes>
           <Route path="/" Component={Home} />
-          <Route path="/month-statistics" Component={MonthStatistics} />
         </Routes>
       </div>
     </>
